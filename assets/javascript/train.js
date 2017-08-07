c961af3478dce19653fad7b94e8ae9f37c30207b
msgctcheng <msgctcheng@gmail.com>
2017-08-07 00:24:04 -0400

    Project Requrements Finished

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -6,7 +6,6 @@ var config = {
     storageBucket: "train-41371.appspot.com",
     messagingSenderId: "316413168468"
   };
-
 firebase.initializeApp(config);
 $(document).ready(function(){
   $('.modal').modal({
@@ -19,23 +18,29 @@ var trainDest = "";
 var firstTime = "";
 var trainFreq = 0;
 var currentTime = moment();
+
+$(document).on("click", ".adder", function () {
+  $("#trainName").val("");
+  $("#destiNation").val("");
+  $("#startTime").val("");
+  $("#trainFreq").val("");
+});
 // Validation Functions - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function checkTime(){
-    var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test($("#startTime").val());
-    if (isValid) {
-      $("#startTime").addClass("green lighten-3")
-      setTimeout(function(){
-        $("#startTime").removeClass("green lighten-3")
-      }, 2000);
-    } else {
-      $("#startTime").addClass("red lighten-3");
-      setTimeout(function(){
-        $("#startTime").removeClass("red lighten-3")
-      }, 750);
-    }
-    return isValid;
+  var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test($("#startTime").val());
+  if (isValid) {
+    $("#startTime").addClass("green lighten-3")
+    setTimeout(function(){
+      $("#startTime").removeClass("green lighten-3")
+    }, 2000);
+  } else {
+    $("#startTime").addClass("red lighten-3");
+    setTimeout(function(){
+      $("#startTime").removeClass("red lighten-3")
+    }, 750);
+  }
+  return isValid;
 };
-
 function notEmpty(){
   var checkName;
   var checkDest;
@@ -79,22 +84,6 @@ function notEmpty(){
     return false;
   }
 };
-
-// database.ref().on("child_added", function(childSnapshot) {
-//     var formatTime = moment(childSnapshot.val().firstTime, "HH:mm").subtract(1, "years");
-//     console.log(formatTime);
-//     var timeDiff = moment().diff(moment(formatTime), "minutes");
-//     console.log("time: " + timeDiff);
-//     var remTime = timeDiff % childSnapshot.val().trainFreq;
-//     console.log("remainder: " + remTime);
-//     var timeTill = trainFreq - remTime;
-//     console.log("time till: " + timeTill);
-
-//     var nextTime = moment().add(timeTill, "minutes");
-//     console.log(nextTime)
-    //$("#appendHere").append("<div class='chip row'><div class='new name col s3'>" + snap.val().trainName + "</div><div class='new dest col s3'>" + snap.val().trainDest + "</div><div class='new freq col s2'>" + nextTime + "</div><div class='new next col s2'>" + timeTill + "</div><div class='new away col s2'>1000</div></div>");
-
-  // });      
 //#submitButton onclick
 $(document).on("click", ".subButton", function() {
   if(checkTime() && notEmpty()) {
@@ -102,38 +91,27 @@ $(document).on("click", ".subButton", function() {
     trainDest = $("#destiNation").val().trim();
     firstTime = $("#startTime").val().trim();
     trainFreq = $("#trainFreq").val();
-   
 
-    
-  //push values to firebase
   dataRef.ref().push({
     trainName: trainName,
     trainDest: trainDest,
     firstTime: firstTime,
     trainFreq: trainFreq
   });
-  //pull vals from firebase and store
-  
-  //format time
-  
-  //calculate Next Arrival and Minutes - Store values
-  //append new chip w/ values from virebase and calculated vals
   }
-  
- $("#form").modal("close");
+  $("#form").modal("close");
 });
-
 dataRef.ref().on("child_added", function(childSnapshot) {
     var displayName = childSnapshot.val().trainName;
     var displayDest = childSnapshot.val().trainDest;
     var firstArr = childSnapshot.val().firstTime;
     var frequent = childSnapshot.val().trainFreq;
-    console.log(displayName);
-    console.log(displayDest);
-    console.log(firstArr);
-    console.log(frequent);
-    //$("#appendHere").append("<div class='chip row'><div class='new name col s3'>" + snap.val().trainName + "</div><div class='new dest col s3'>" + snap.val().trainDest + "</div><div class='new freq col s2'>" + nextTime + "</div><div class='new next col s2'>" + timeTill + "</div><div class='new away col s2'>1000</div></div>");
-
+    var convertedTime = moment(firstArr, "HH:mm").subtract(1, "years");
+    var timeDiff = moment().diff(moment(convertedTime), "minutes");
+    var remTime = timeDiff % frequent;
+    var timeTill = frequent - remTime;
+    var nextTime = moment().add(timeTill, "minutes");
+    $("#appendHere").append("<div class='chip row center-align'><div class='new name col s3'>" + displayName + "</div><div class='new dest col s3'>" + displayDest + "</div><div class='new freq col s2'>" + frequent + "</div><div class='new next col s2'>" + moment(nextTime).format("HH:mm") + "</div><div class='new away col s2'>" + timeTill + "</div></div>");
   });      
  
   
